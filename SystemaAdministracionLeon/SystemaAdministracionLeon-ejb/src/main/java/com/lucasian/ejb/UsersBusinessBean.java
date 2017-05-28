396d0a72462836541df140ababe88ec0b2649842
Leroy Alan Rodríguez <alan.rodriguez.villar@gmail.com>
2017-05-28 18:04:39 -0500

    Cambio en usuarios

--- SystemaAdministracionLeon/SystemaAdministracionLeon-ejb/src/main/java/com/lucasian/ejb/UsersBusinessBean.java
+++ SystemaAdministracionLeon/SystemaAdministracionLeon-ejb/src/main/java/com/lucasian/ejb/UsersBusinessBean.java
@@ -24,14 +24,14 @@ public class UsersBusinessBean implements UsersBusinessBeanRemote {
     public List<Map<String,String>> businessMethod() {       
        List<Map<String,String>> listOfMaps = new ArrayList<>();
        Map<String,String> map = null;
-       final int limit = 10;
+       final int limit = 1000;
         for(int x = 0 ; x < limit ; x++){
             map = new HashMap<>();
             map.put("usuario",x%2==0 ? "Alan"+x : x%6 == 0 ? "Pedro"+x : "Leroy "+x );
-            map.put("equipo",x%2==0 ? "Lenovo"+x : x%6 == 0 ? "Mac Pro"+x : "HP "+x );
-            map.put("plataforma",x%2==0 ? "Linux"+x : x%6 == 0 ? "WinDOS"+x : "HP UX "+x );
-            map.put("edad",x%2==0 ? "45"+x : x%6 == 0 ? "24"+x : "37 "+x );
-            map.put("salario",x%2==0 ? "30,000"+x : x%6 == 0 ? "13,899"+x : "32,500 "+x );
+            map.put("equipo",x%2==0 ? "Lenovo" : x%6 == 0 ? "Mac Pro" : "HP " );
+            map.put("plataforma",x%2==0 ? "Linux" : x%6 == 0 ? "WinDOS" : "HP UX " );
+            map.put("edad",x%2==0 ? "45" : x%6 == 0 ? "24" : "37 " );
+            map.put("salario",x%2==0 ? "30,000" : x%6 == 0 ? "13,899" : "32,500 " );
             listOfMaps.add(map);
         }
         return listOfMaps;
